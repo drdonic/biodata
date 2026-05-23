84566ce6ce4e61d9f11d7a67eaf306abacef3954
drdonic <luchaamrep680@hotmail.com>
2026-05-23 11:46:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 st.title("AAA")
 st.header("About Me")
-image=Image.open(r"C:\Users\pjh60\Desktop\作业\PJH.png")
+image=Image.open("PJH.png")
 st.image(image,caption="My Picture",width=250)
 # st.image("https://upload.wikimedia.org/wikipedia/commons/8/8d/Google_logo_%282010-2013%29.svg", width=150)
 
